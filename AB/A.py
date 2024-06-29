91cef00128c40af131989f687f310c92c695d9ef
Ruchi Singh <141247708+Ruchisingh13@users.noreply.github.com>
2024-06-29 14:50:37 +0530

    Add files via upload

---
+++ AB/A.py
@@ -0,0 +1,4 @@
+student_name = ["Rocky","Rohit"]
+
+if __name__ == "__main__":
+    collage = "iit lacknow"
---
+++ AB/B.py
@@ -0,0 +1,15 @@
+# import A
+# print("student:",A.student_name)
+# print("college:",A.collage)
+
+
+
+from A import student_name,collage
+from A import collage
+print()
+print("student name:",student_name)
+print("college:",collage)
+
+print()
+
+print(collage)
